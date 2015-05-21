5031d9eb6e4b3ebc568cb6b45fe85fb107da1a19
liusuwang <liusu.wang@gmail.com>
2015-05-21 04:19:54 -0700

    table

--- js/detail.js
+++ js/detail.js
@@ -6,20 +6,49 @@ function explodeDetails(d) {
     var fileName = "HIV_pt1_AA_align_tp1";
     var url = "http://192.241.216.102/512-finalProject/query.php?file=" + fileName + "&index=" + currIndex + "&request=json";
     d3.json(url, function(error, data) {
-        
+		
+        var old_table = document.getElementById("details");
+		var table_div = old_table.cloneNode();
+	
+		/*
+		var header = table_div.createTHead();
+		var row = header.insertRow(0);
+		var cell = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+		cell.innerHTML = "<b>Day</b>";
+		cell2.innerHTML = "<b>AA</b>";
+		*/
         for (var i = 0; i < data.length; i++) {
+			
             var currDay = data[i].V;
             var currAAs = data[i].aa;
+			var tr = document.createElement('tr');
+			var td = document.createElement('td');	
+			
+			//td.setAttribute("rowspan", currAAs.length-1);
+			td.appendChild(document.createTextNode(currDay));
+			tr.appendChild(td);
+			//tr.appendChild(document.createTextNode(currDay));
+			
+			
+			for (var j = 0; j < currAAs.length; j++){
+				
+				//var td = document.createElement('td');
 
-            // 大概就从这儿开始。。。
+				tr.appendChild(document.createTextNode(currAAs[j]));
+				//tr.appendChild(td);
+				table_div.appendChild(tr);
+			}
+
+            
             console.log(currDay);
             console.log(currAAs);
 
 
         }
-
-        var table_div = document.getElementById("details");
-        table_div.appendChild();
+		old_table.parentNode.replaceChild(table_div,old_table);
+        
+        
     });
   
 
