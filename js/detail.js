c1a43928a6d8226534a4652a01b5619d0f5af690
liusuwang <liusu.wang@gmail.com>
2015-05-23 19:02:41 -0700

    updated table and style

--- js/detail.js
+++ js/detail.js
@@ -7,45 +7,55 @@ function explodeDetails(d) {
     var url = "http://192.241.216.102/512-finalProject/query.php?file=" + fileName + "&index=" + currIndex + "&request=json";
     d3.json(url, function(error, data) {
 		
-        var old_table = document.getElementById("details");
+        var old_table = document.getElementById("time_line");
+		//time_line.style.fontSize = "8px";
+		//time_line.style.border = "thin solid #6f6d6d";
+		//time_line.style.borderColor = "black";
+		
 		var table_div = old_table.cloneNode();
-	
-		/*
+
+		table_div.setAttribute("BGCOLOR","#ffffff");
+		
 		var header = table_div.createTHead();
 		var row = header.insertRow(0);
 		var cell = row.insertCell(0);
 		var cell2 = row.insertCell(1);
 		cell.innerHTML = "<b>Day</b>";
 		cell2.innerHTML = "<b>AA</b>";
-		*/
+		cell.style.paddingLeft = "4px";
+		cell.style.paddingRight = "4px";
+		cell2.style.paddingLeft = "4px";
+		cell2.style.paddingRight = "4px";
+		
         for (var i = 0; i < data.length; i++) {
 			
             var currDay = data[i].V;
             var currAAs = data[i].aa;
 			var tr = document.createElement('tr');
+			var th = document.createElement('th');	
 			var td = document.createElement('td');	
-			
-			//td.setAttribute("rowspan", currAAs.length-1);
-			td.appendChild(document.createTextNode(currDay));
-			tr.appendChild(td);
-			//tr.appendChild(document.createTextNode(currDay));
-			
-			
-			for (var j = 0; j < currAAs.length; j++){
-				
-				//var td = document.createElement('td');
 
-				tr.appendChild(document.createTextNode(currAAs[j]));
-				//tr.appendChild(td);
+			th.setAttribute("rowspan", currAAs.length);
+			th.appendChild(document.createTextNode(currDay));
+			td.appendChild(document.createTextNode(currAAs[0]));	
+			tr.appendChild(th);
+			tr.appendChild(td);
+		
+			table_div.appendChild(tr);			
+			
+			for (var j = 1; j < currAAs.length; j++){				
+				var tr = document.createElement('tr');
+				var td = document.createElement('td');
+
+				td.appendChild(document.createTextNode(currAAs[j]));
+				tr.appendChild(td);
 				table_div.appendChild(tr);
 			}
-
-            
+			
             console.log(currDay);
             console.log(currAAs);
-
-
         }
+	
 		old_table.parentNode.replaceChild(table_div,old_table);
         
         
